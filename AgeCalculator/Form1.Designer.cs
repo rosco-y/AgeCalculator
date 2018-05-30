07f3cc7cf65567b4f67c689d443e47b33301de67
Ross Ylitalo <ross.ylitalo@gmail.com>
2018-05-30 11:37:11 -0400

    Working model:  Displays Years, Months, Weeks, Days, Hours, Minutes and Seconds old.  Does not display time before next birthday.

--- AgeCalculator/Form1.Designer.cs
+++ AgeCalculator/Form1.Designer.cs
@@ -29,12 +29,64 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.tmrAging = new System.Windows.Forms.Timer(this.components);
+            this.dtBirthDate = new System.Windows.Forms.DateTimePicker();
+            this.cmdCalculateAge = new System.Windows.Forms.Button();
+            this.txtAgeReport = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tmrAging
+            // 
+            this.tmrAging.Interval = 1000;
+            this.tmrAging.Tick += new System.EventHandler(this.tmrAging_Tick);
+            // 
+            // dtBirthDate
+            // 
+            this.dtBirthDate.Location = new System.Drawing.Point(163, 33);
+            this.dtBirthDate.Name = "dtBirthDate";
+            this.dtBirthDate.Size = new System.Drawing.Size(200, 20);
+            this.dtBirthDate.TabIndex = 0;
+            // 
+            // cmdCalculateAge
+            // 
+            this.cmdCalculateAge.Location = new System.Drawing.Point(288, 59);
+            this.cmdCalculateAge.Name = "cmdCalculateAge";
+            this.cmdCalculateAge.Size = new System.Drawing.Size(75, 23);
+            this.cmdCalculateAge.TabIndex = 1;
+            this.cmdCalculateAge.Text = "&OK";
+            this.cmdCalculateAge.UseVisualStyleBackColor = true;
+            this.cmdCalculateAge.Click += new System.EventHandler(this.cmdCalculateAge_Click);
+            // 
+            // txtAgeReport
+            // 
+            this.txtAgeReport.Font = new System.Drawing.Font("Segoe UI Black", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAgeReport.Location = new System.Drawing.Point(163, 113);
+            this.txtAgeReport.Multiline = true;
+            this.txtAgeReport.Name = "txtAgeReport";
+            this.txtAgeReport.Size = new System.Drawing.Size(447, 291);
+            this.txtAgeReport.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(831, 604);
+            this.Controls.Add(this.txtAgeReport);
+            this.Controls.Add(this.cmdCalculateAge);
+            this.Controls.Add(this.dtBirthDate);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer tmrAging;
+        private System.Windows.Forms.DateTimePicker dtBirthDate;
+        private System.Windows.Forms.Button cmdCalculateAge;
+        private System.Windows.Forms.TextBox txtAgeReport;
     }
 }
 
--- AgeCalculator/Form1.cs
+++ AgeCalculator/Form1.cs
@@ -12,9 +12,47 @@ namespace AgeCalculator
 {
     public partial class Form1 : Form
     {
+        const double ApproxDaysPerMonth = 30.4375;
+        const double ApproxDaysPerYear = 365.25;
+        DateTime _birthDate;
+        StringBuilder ageReport;
+
         public Form1()
         {
             InitializeComponent();
+            ageReport = new StringBuilder();
+        }
+
+        private void cmdCalculateAge_Click(object sender, EventArgs e)
+        {
+            _birthDate = dtBirthDate.Value;
+            tmrAging.Enabled = true;
+        }
+
+        private void tmrAging_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+
+            TimeSpan totalTime = DateTime.Now.Subtract(_birthDate);
+
+            int iDays = (int)totalTime.TotalDays;
+            int iYear = (int)(iDays / ApproxDaysPerYear);
+            int iMonths = (int)(iYear * 12);
+            int iWeeks = (int)(iMonths / ApproxDaysPerMonth) * 7;
+            int iHours = (int)totalTime.TotalHours;
+            int iMinutes = (int)totalTime.TotalHours;
+            int iSeconds = (int)totalTime.TotalSeconds;
+
+            //StringBuilder ageReport = new StringBuilder();
+            ageReport.Clear();
+            ageReport.Append($"You are {iYear:N0} years old..." + Environment.NewLine + Environment.NewLine);
+            ageReport.Append($"\tor {iMonths:N0} months old" + Environment.NewLine);
+            ageReport.Append($"\tor {iWeeks:N0} weeks old" + Environment.NewLine);
+            ageReport.Append($"\tor {iDays:N0} days old" + Environment.NewLine);
+            ageReport.Append($"\tor {iHours:N0} hours old" + Environment.NewLine);
+            ageReport.Append($"\tor {iMinutes:N0} minutes old" + Environment.NewLine);
+            ageReport.Append($"\tor {iSeconds:N0} seconds old." + Environment.NewLine);
+            txtAgeReport.Text = ageReport.ToString();
         }
     }
 }
